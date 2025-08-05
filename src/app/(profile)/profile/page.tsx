8501a11dbda030a1819403f4334db17cd358e31f
Gopal Sarkar <gopalsarkar.dev@gmail.com>
2025-08-05 09:43:39 +0530

    Update page.tsx

--- src/app/(profile)/profile/page.tsx
+++ src/app/(profile)/profile/page.tsx
@@ -1,4 +1,20 @@
 import UserProfile from "@/components/profile/UserProfile";
+import { Metadata } from "next";
+
+export const generateMetadata: () => Metadata = () => ({
+	title: "Nodesnap | Profile",
+	description:
+		"Nodesnap is a full-stack progect built on Microservices architecture",
+	keywords: [
+		"Nodesnap",
+		"Snapshot",
+		"Nodejs project",
+		"Full-Stack Project",
+		"Nextjs-Project",
+		"Post a Snapshot",
+		"Profile Page",
+	],
+});
 
 const page = () => {
 	return (
