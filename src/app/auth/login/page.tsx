051f83912a01c29d12c785fe1f0b3ea6bb0e0df1
Gopal Sarkar <gopalsarkar.dev@gmail.com>
2025-08-05 09:34:09 +0530

    Update page.tsx

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,4 +1,20 @@
 import LoginForm from "@/components/auth/LoginForm";
+import { Metadata } from "next";
+
+export const generateMetadata: () => Metadata = () => ({
+	title: "Nodesnap | Login",
+	description:
+		"Nodesnap is a full-stack progect built on Microservices architecture",
+	keywords: [
+		"Nodesnap",
+		"Snapshot",
+		"Nodejs project",
+		"Full-Stack Project",
+		"Nextjs-Project",
+		"Post a Snapshot",
+		"Login Page",
+	],
+});
 
 const page = () => {
 	return (
