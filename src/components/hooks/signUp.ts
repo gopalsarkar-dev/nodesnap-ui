2dd5f480848da8e5f1479f8c6e2326fe9162af38
Gopal Sarkar <gopalsarkar.dev@gmail.com>
2025-08-07 23:20:35 +0530

    User Successfully register

---
+++ src/components/hooks/signUp.ts
@@ -0,0 +1,63 @@
+import kyClient from "@/lib/ky/kyClient";
+import { Data, DirectusResponse, SignupFormType } from "@/lib/type";
+import { HTTPError } from "ky";
+
+const signUp = async (sInfo: SignupFormType) => {
+	// console.log(sInfo);
+	try {
+		const { data } = await kyClient
+			.get(`users`, {
+				next: { tags: ["signUp"] },
+				searchParams: {
+					filter: JSON.stringify({
+						email: {
+							_eq: sInfo.email,
+						},
+					}),
+				},
+			})
+			.json<DirectusResponse<Data[]>>();
+
+		if (data.length === 0) {
+			await kyClient.post(`users/register`, {
+				json: sInfo,
+			});
+
+			return {
+				success: true,
+				message: "User Successfully SignUp",
+			};
+		} else {
+			return {
+				success: false,
+				message: `${sInfo.email} already exists`,
+			};
+		}
+		console.log(data);
+	} catch (error) {
+		if (error instanceof HTTPError) {
+			const respos = await error.response.json<{
+				error: { message: string }[];
+			}>();
+
+			if (error.response.status === 400 || error.response.status === 401) {
+				return {
+					success: false,
+					message: "Unauthorized User",
+				};
+			}
+
+			return {
+				success: false,
+				message: respos.error[0].message,
+			};
+		} else {
+			return {
+				success: false,
+				message: "Network Error. Please check your connection.",
+			};
+		}
+	}
+};
+
+export default signUp;
--- src/lib/type.ts
+++ src/lib/type.ts
@@ -10,3 +10,11 @@ export type LoginFormType = z.infer<typeof loginFormSchema>;
 export type SignupFormType = z.infer<typeof signupFormSchema>;
 export type EditProfileType = z.infer<typeof editProfileSchema>;
 export type UserpostType = z.infer<typeof userPostSchema>;
+
+export type DirectusResponse<D> = {
+	data: D;
+};
+
+export type Data = {
+	data: string;
+};
