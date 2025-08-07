bb39d6a02e0447ab5236963d729d2bd66f014874
Gopal Sarkar <gopalsarkar.dev@gmail.com>
2025-08-07 12:48:46 +0530

    React-toastify for notified

--- src/components/theme/ThemeProvider.tsx
+++ src/components/theme/ThemeProvider.tsx
@@ -1,10 +1,16 @@
 "use client";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import ReactToastify from "../ReactToastify";
 
 type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
 
 const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
-	return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+	return (
+		<NextThemesProvider {...props}>
+			{children}
+			<ReactToastify />
+		</NextThemesProvider>
+	);
 };
 
 export default ThemeProvider;
