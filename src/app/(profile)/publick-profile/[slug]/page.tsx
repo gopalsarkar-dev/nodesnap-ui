e1373b73fb22315dc33c6cdaaa561b8bf5a5a6df
Gopal Sarkar <gopalsarkar.dev@gmail.com>
2025-08-20 13:54:57 +0530

    public-profile 🛩️

--- src/app/(profile)/publick-profile/[slug]/page.tsx
+++ src/app/(profile)/publick-profile/[slug]/page.tsx
@@ -1,7 +1,21 @@
-const page = () => {
+import singleUserId from "@/components/hooks/post/singleUserId";
+import PublickProfileCard from "@/components/profile/publickprofile/PublickProfileCard";
+
+type IdProps = {
+	params: Promise<{ slug: string }>;
+};
+
+const page = async ({ params }: IdProps) => {
+	const { slug } = await params;
+
+	const { data } = await singleUserId(slug);
+	if (data === null) {
+		return <></>;
+	}
+
 	return (
 		<>
-			<div className="">Publick-profile</div>
+			<PublickProfileCard pInfo={data} />
 		</>
 	);
 };
